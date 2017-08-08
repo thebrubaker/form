07d2155fbfa74b20af74ef977c17581f13984a14
Joel Brubaker <pk.joel@gmail.com>
2017-08-08 14:14:57 -0700

    Continued updates

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,7 @@
 let fields = Symbol('fields')
 let errors = Symbol('errors')
 let classes = Symbol('classes')
+let validation = Symbol('validation')
 
 let defaults = {
   applyClassNames: true,
@@ -53,7 +54,134 @@ class Form {
    * Initialize the reactive data on the form.
    */
   initReactiveData () {
+    let validator = new Vue({
+      data () {
+        return {
+          rules: {
+            name: {
+              required: (value) => {
+                return new Promise((resolve, reject) => {
+                  if (value !== undefined && value !== null && value !== '') {
+                    resolve({ 
+                      rule: 'required',
+                      isValid: true 
+                    })
+                  } else {
+                    reject({ 
+                      rule: 'required',
+                      isValid: false 
+                    })
+                  }
+                })
+              },
+              unique: (value) => {
+                return new Promise((resolve, reject) => {
+                  if (value !== 'Product') {
+                    resolve({ 
+                      rule: 'unique',
+                      isValid: true 
+                    })
+                  } else {
+                    reject({ 
+                      rule: 'unique',
+                      isValid: false 
+                    })
+                  }
+                })
+              }
+            }
+          }
+        }
+      },
+      methods: {
+        attempt (field, value) {
+          return Promise.all(
+            Object.keys(this.rules[field]).map( rule => this.rules[field][rule](value) )
+          )
+        }
+      }
+    })
+    let fields = new Vue({
+      data () {
+        return {
+          fields: {
+            name: {
+              value: '',
+              originalValue: '',
+              isValid: false,
+              isModified: false,
+              isDirty: false
+            },
+            sku: {
+              value: '',
+              originalValue: '',
+              isValid: false,
+              isModified: false,
+              isDirty: false
+            }
+          }
+        }
+      },
+      computed: {
+        name: {
+          get () {
+            return this.fields['name'].value
+          },
+          set (value) {
+            validator.attempt('name', value).then(({ isValid, errors }) => {
+              this.fields['name'].value = value
+              this.fields['name'].isValid = isValid
+              this.fields['name'].isModified = value !== this.fields['name'].originalValue
+              this.fields['name'].isDirty = true
+              errors.push('name', errors)
+            })
+          }
+        },
+        sku: {
+          get () {
+            return this.fields['sku'].value
+          },
+          set (value) {
+            validator.attempt('sku', value).then(result => {
+              this.fields['sku'].value = value
+              this.fields['sku'].isValid = result.isValid
+              this.fields['sku'].isModified = value !== this.fields['sku'].originalValue
+              this.fields['sku'].isDirty = true
+              this.setErrors('sku', result)
+            })
+          }
+        }
+      },
+      methods: {
+        validate (validators, value) {
+          return Promise.all(validators).then(results => {
 
+          })
+        },
+        setErrors (key, errors) {
+
+        }
+      }
+    })
+    let errors = new Vue({
+      data () {
+        return {
+          bag: {
+            name: {
+              required: 'This field is required.',
+              unique: 'This name is already taken.'
+            },
+            sku: {
+              required: 'This field is required.',
+              unique: 'This sku is already taken.'
+            }
+          }
+        }
+      },
+      methods: {
+        set ()
+      }
+    })
   }
 
   /**
